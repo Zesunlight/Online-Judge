1e9279c2c291445a7453f94dbe46d2838d5d9156
Zesunlight <2714616793@qq.com>
2016-12-13 16:19:08 +0800

    Add files via upload

--- CDOJ/1515-保护果实.c
+++ CDOJ/1515-保护果实.c
@@ -24,3 +24,36 @@ int main()
 		printf("-1");
 	return 0;
 }
+
+---------------------------------------------------
+
+#include <iostream>
+#include<cstdio>
+using namespace std;
+
+int a[1000005];
+
+int main()
+{
+    int n,i,m;
+    int s;
+    scanf("%d",&n);
+    for(i = 0; i < n; i++) scanf("%d",&a[i]);
+    if(n < 3) {printf("-1\n"); return 0;}
+    m = max(a[0],a[1]);
+    s = min(a[0],a[1]);
+    for(i = 2; i < n; i++)
+    {
+        if(a[i] > m)
+        {
+            s += m;
+            m = a[i];
+        }
+        else s += a[i];
+        if(s > m) break;
+    }
+    if(i < n) printf("%d\n",i + 1);
+    else printf("-1\n");
+    return 0;
+}
+
