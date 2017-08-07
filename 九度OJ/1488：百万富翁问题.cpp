23164729a38794d6c54a3fa1c6af7a0b5de35024
ZYZ <ze0975@163.com>
2017-08-07 23:02:08 +0800

    practise

---
+++ 九度OJ/1488：百万富翁问题.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+
+    long long int n = 1;
+    n = n << 30;
+    printf("300 %lld\n", n - 1);
+
+    return 0;
+}
+
+/****************************************************
+    Problem: 1488
+    Website: http://ac.jobdu.com/problem.php?pid=1488
+    User: ZYZ
+    Language: C++
+    Result: Accepted
+    Time: 0 ms
+    Memory: 1516 kb
+*****************************************************/
