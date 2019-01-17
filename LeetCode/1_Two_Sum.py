3fa255c04f9e7189a0ccca10857bf7e4d8bd7175
二木头 <2714616793@qq.com>
2019-01-17 11:08:59 +0800

    Create 1_Two_Sum.py

---
+++ LeetCode/1_Two_Sum.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+	def __init__(self):
+		self.name = 'leetcode'
+
+	def findIndices(self, nums, target):
+		length = len(nums)
+		for i in range(length):
+			for j in range(i+1, length):
+				if nums[i] + nums[j] == target:
+					return [i, j]
+
+	def getInformation(self):
+		print(self)
+
+if __name__ == '__main__':
+	s = Solution()
+	print(s.findIndices([2, 7, 11, 15], 9))
