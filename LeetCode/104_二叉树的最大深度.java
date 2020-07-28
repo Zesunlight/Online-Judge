003a7727149a41e982afe7b0a3c1734793f3df40
赵永泽 <2714616793@qq.com>
2020-07-28 08:30:53 +0800

    daily

---
+++ LeetCode/104_二叉树的最大深度.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+/*
+    104. 二叉树的最大深度
+    https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+
+    给定一个二叉树，找出其最大深度。
+    二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+    说明: 叶子节点是指没有子节点的节点。
+
+    执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗：39.7 MB, 在所有 Java 提交中击败了5.75%的用户
+ */
+
+
+//**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
+
+class Solution2 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/solution/er-cha-shu-de-zui-da-shen-du-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
