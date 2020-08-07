f44912c452982ea1f7246bf9bb9fd669669dd5d3
赵永泽 <2714616793@qq.com>
2020-08-07 13:46:57 +0800

    daily

---
+++ LeetCode/100_相同的树.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+/*
+    100. 相同的树
+    https://leetcode-cn.com/problems/same-tree/
+
+    给定两个二叉树，编写一个函数来检验它们是否相同。
+    如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+    执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗：37.1 MB, 在所有 Java 提交中击败了68.83%的用户
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        if (isSameTree(p.left, q.left) && isSameTree(p.right, q.right)) return true;
+        return false;
+    }
+}
+
+class Solution2 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/same-tree/solution/xiang-tong-de-shu-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
