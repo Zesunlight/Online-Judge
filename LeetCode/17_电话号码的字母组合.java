9437ffde01c050368b63732b2ec335a9d3958737
赵永泽 <2714616793@qq.com>
2020-08-26 10:35:18 +0800

    daily

---
+++ LeetCode/17_电话号码的字母组合.java
@@ -0,0 +1,107 @@
+import java.util.*;
+
+/*
+    17. 电话号码的字母组合
+    https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+
+    给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+    给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+    执行用时：2 ms, 在所有 Java 提交中击败了45%的用户
+    内存消耗：38.6 MB, 在所有 Java 提交中击败了39%的用户
+ */
+
+class Solution {
+    Map<Character, char[]> map = new HashMap<>();
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) return new ArrayList<>();
+
+        map.put('2', new char[] {'a', 'b', 'c'});
+        map.put('3', new char[] {'d', 'e', 'f'});
+        map.put('4', new char[] {'g', 'h', 'i'});
+        map.put('5', new char[] {'j', 'k', 'l'});
+        map.put('6', new char[] {'m', 'n', 'o'});
+        map.put('7', new char[] {'p', 'q', 'r', 's'});
+        map.put('8', new char[] {'t', 'u', 'v'});
+        map.put('9', new char[] {'w', 'x', 'y', 'z'});
+
+        return letterCombinations(digits.toCharArray(), 0);
+    }
+
+    public List<String> letterCombinations(char[] digits, int start) {
+        if (start == digits.length) {
+            return new ArrayList<String>(){{add("");}};
+        }
+        List<String> part = new ArrayList<>();
+        for (String s : letterCombinations(digits, start + 1)) {
+            for (Character c : map.get(digits[start])) {
+                part.add(c + s);
+            }
+        }
+        return part;
+    }
+}
+
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<String>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+
+    public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
+    }
+}
+
+
+/*
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+        
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        groups = (phoneMap[digit] for digit in digits)
+        return ["".join(combination) for combination in itertools.product(*groups)]
+*/
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/solution/dian-hua-hao-ma-de-zi-mu-zu-he-by-leetcode-solutio/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
