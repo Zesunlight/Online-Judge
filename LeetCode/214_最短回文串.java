e032e8e64595abf5cdcdc4d3ab47c6ca9fe4cf67
赵永泽 <2714616793@qq.com>
2020-08-29 17:01:53 +0800

    daily

---
+++ LeetCode/214_最短回文串.java
@@ -0,0 +1,68 @@
+/*
+    214. 最短回文串
+    https://leetcode-cn.com/problems/shortest-palindrome/
+
+    给定一个字符串 s，你可以通过在字符串前面添加字符将其转换为回文串。
+    找到并返回可以用这种方式转换的最短回文串。
+
+    执行用时： ms, 在所有 Java 提交中击败了 的用户
+    内存消耗： MB, 在所有 Java 提交中击败了 的用户
+ */
+
+
+class Solution {
+    public String shortestPalindrome(String s) {
+        int n = s.length();
+        int base = 131, mod = 1000000007;
+        int left = 0, right = 0, mul = 1;
+        int best = -1;
+        for (int i = 0; i < n; ++i) {
+            left = (int) (((long) left * base + s.charAt(i)) % mod);
+            right = (int) ((right + (long) mul * s.charAt(i)) % mod);
+            if (left == right) {
+                best = i;
+            }
+            mul = (int) ((long) mul * base % mod);
+        }
+        String add = (best == n - 1 ? "" : s.substring(best + 1));
+        StringBuffer ans = new StringBuffer(add).reverse();
+        ans.append(s);
+        return ans.toString();
+    }
+}
+
+
+class Solution2 {
+    public String shortestPalindrome(String s) {
+        int n = s.length();
+        int[] fail = new int[n];
+        Arrays.fill(fail, -1);
+        for (int i = 1; i < n; ++i) {
+            int j = fail[i - 1];
+            while (j != -1 && s.charAt(j + 1) != s.charAt(i)) {
+                j = fail[j];
+            }
+            if (s.charAt(j + 1) == s.charAt(i)) {
+                fail[i] = j + 1;
+            }
+        }
+        int best = -1;
+        for (int i = n - 1; i >= 0; --i) {
+            while (best != -1 && s.charAt(best + 1) != s.charAt(i)) {
+                best = fail[best];
+            }
+            if (s.charAt(best + 1) == s.charAt(i)) {
+                ++best;
+            }
+        }
+        String add = (best == n - 1 ? "" : s.substring(best + 1));
+        StringBuffer ans = new StringBuffer(add).reverse();
+        ans.append(s);
+        return ans.toString();
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/shortest-palindrome/solution/zui-duan-hui-wen-chuan-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
