74f35a74cbe3e268d200250a0eef967314f9ef50
赵永泽 <2714616793@qq.com>
2020-10-13 16:58:32 +0800

    daily

---
+++ LeetCode/24_两两交换链表中的节点.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+/*
+    24. 两两交换链表中的节点
+    https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+
+    给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+    你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+    执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗：36 MB, 在所有 Java 提交中击败了99.51%的用户
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) return null;
+        ListNode fake = new ListNode(0, head);
+        ListNode pre = fake, cur = head;
+        while (cur != null && cur.next != null) {
+            pre.next = cur.next;
+            ListNode temp = cur.next.next;
+            cur.next.next = cur;
+            cur.next = temp;
+            pre = cur;
+            cur = cur.next;
+        }
+        return fake.next;
+    }
+}
+
+
+class Solution2 {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-91/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+class Solution3 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-91/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
