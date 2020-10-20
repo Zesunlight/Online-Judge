94bcbfc8df02302cce2bcdf4f113f537ede3fe47
赵永泽 <2714616793@qq.com>
2020-10-20 10:21:57 +0800

    daily

---
+++ LeetCode/143_重排链表.java
@@ -0,0 +1,132 @@
+import java.util.*;
+
+/*
+    143. 重排链表
+    https://leetcode-cn.com/problems/reorder-list/
+
+    给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+    将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+    执行用时：3 ms, 在所有 Java 提交中击败了36.63%的用户
+    内存消耗：40.8 MB, 在所有 Java 提交中击败了98.07%的用户
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+
+        int n = list.size();
+        if (n <= 2) return;
+        list.get(0).next = list.get(n - 1);
+        ListNode pre = list.get(n - 1);
+
+        for (int i = 1; i < n / 2; i++) {
+            pre.next = list.get(i);
+            pre.next.next = list.get(n - i - 1);
+            pre = pre.next.next;
+        }
+        if (n % 2 == 1) {
+            pre.next = list.get((n - 1) / 2);
+            pre = pre.next;
+        }
+        pre.next = null;
+    }
+}
+
+
+class Solution2 {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        List<ListNode> list = new ArrayList<ListNode>();
+        ListNode node = head;
+        while (node != null) {
+            list.add(node);
+            node = node.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
+}
+
+
+class Solution3 {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode mid = middleNode(head);
+        ListNode l1 = head;
+        ListNode l2 = mid.next;
+        mid.next = null;
+        l2 = reverseList(l2);
+        mergeList(l1, l2);
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public void mergeList(ListNode l1, ListNode l2) {
+        ListNode l1_tmp;
+        ListNode l2_tmp;
+        while (l1 != null && l2 != null) {
+            l1_tmp = l1.next;
+            l2_tmp = l2.next;
+
+            l1.next = l2;
+            l1 = l1_tmp;
+
+            l2.next = l1;
+            l2 = l2_tmp;
+        }
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/reorder-list/solution/zhong-pai-lian-biao-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
