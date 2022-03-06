f2d119cde3f6f258ef76edbaaa89541c76eb9964
Misaki <2714616793@qq.com>
2022-03-06 16:19:38 +0800

    Update 2100_适合打劫银行的日子.py

--- LeetCode/2100_适合打劫银行的日子.py
+++ LeetCode/2100_适合打劫银行的日子.py
@@ -46,3 +46,15 @@ class Solution:
                 res.append(i)
 
         return res
+
+    def goodDaysToRobBank2(self, security: List[int], time: int) -> List[int]:
+        # https://leetcode-cn.com/problems/find-good-days-to-rob-the-bank/solution/gua-he-da-jie-yin-xing-de-ri-zi-by-leetc-z6r1/
+        n = len(security)
+        left = [0] * n
+        right = [0] * n
+        for i in range(1, n):
+            if security[i] <= security[i - 1]:
+                left[i] = left[i - 1] + 1
+            if security[n - i - 1] <= security[n - i]:
+                right[n - i - 1] = right[n - i] + 1
+        return [i for i in range(time, n - time) if left[i] >= time and right[i] >= time]
